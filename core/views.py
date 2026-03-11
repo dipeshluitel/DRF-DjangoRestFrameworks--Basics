84309764ece9870f35668ef02d81713e2e0c854c
Dipesh <dipeshluitel40@gmail.com>
2026-03-11 22:20:25 +0545

    post view created and tested success with thunder client

--- core/views.py
+++ core/views.py
@@ -11,3 +11,9 @@ def home(request):
     student_objs = Students.objects.all()
     serializer = StudentsSerializer(student_objs,many= True)
     return Response({'status':200, 'message':'Hello From Django Rest Framework','payload' : serializer.data})
+
+@api_view(['POST'])
+def post_student(request):
+    data = request.data
+    print(data)
+    return Response({'status':200, 'payload':data, 'message':'you sent student data'})
