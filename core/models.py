bb228b48be978f52ec4f1adc852b0f8137879c69
Dipesh <dipeshluitel40@gmail.com>
2026-03-11 21:35:31 +0545

    sample model created

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Students(models.Model):
+    name = models.CharField(max_length=100)
+    age =  models.IntegerField(default=18)
+    father_name = models.CharField(max_length=100)
+    
