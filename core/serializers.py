eb94d37fb515761686eaf0a93eba5e0785b18808
Dipesh <dipeshluitel40@gmail.com>
2026-03-11 21:59:43 +0545

    serializers created and Used it @ view for Get request

---
+++ core/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+from .models import *
+class StudentsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Students
+        fields = ['name', 'age']
--- core/views.py
+++ core/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Students
+from .serializers import *
 
 # Create your views here.
 
-@api_view(["POST","GET"])
+@api_view(["GET"])
 def home(request):
-    return Response({'status':200, 'message':'Hello From Django Rest Framework'})
+    student_objs = Students.objects.all()
+    serializer = StudentsSerializer(student_objs,many= True)
+    return Response({'status':200, 'message':'Hello From Django Rest Framework','payload' : serializer.data})
